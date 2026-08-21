e4c4c15cf924e8f812533ae762b6c623613e0ed4
User <user@example.com>
2026-08-21 23:33:14 +0700

    Expose APP_ENV=local error stack trace

--- api/index.php
+++ api/index.php
@@ -30,7 +30,7 @@ if (file_exists(__DIR__ . '/../public/hot')) {
 
 // 2. Set environment variables for Vercel Serverless
 putenv("VERCEL=1");
-putenv("APP_ENV=production");
+putenv("APP_ENV=local");
 putenv("APP_DEBUG=true");
 putenv("APP_STORAGE_PATH={$tmpStorage}");
 putenv("VIEW_COMPILED_PATH=/tmp/views");
@@ -42,7 +42,7 @@ putenv("APP_MAINTENANCE_STORE=array");
 putenv("ASSET_URL=/public");
 
 $_ENV['VERCEL']                 = '1';
-$_ENV['APP_ENV']                = 'production';
+$_ENV['APP_ENV']                = 'local';
 $_ENV['APP_DEBUG']              = 'true';
 $_ENV['APP_STORAGE_PATH']       = $tmpStorage;
 $_ENV['VIEW_COMPILED_PATH']     = '/tmp/views';
@@ -52,7 +52,7 @@ $_ENV['SESSION_DRIVER']         = 'cookie';
 $_ENV['ASSET_URL']              = '/public';
 
 $_SERVER['VERCEL']                 = '1';
-$_SERVER['APP_ENV']                = 'production';
+$_SERVER['APP_ENV']                = 'local';
 $_SERVER['APP_DEBUG']              = 'true';
 $_SERVER['APP_STORAGE_PATH']       = $tmpStorage;
 $_SERVER['VIEW_COMPILED_PATH']     = '/tmp/views';
